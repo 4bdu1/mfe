35dcaae967b87332e3a7d969481d931bd85b033f
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2022-06-22 15:33:52 +0000

    Add container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const {
   merge
 } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require("../package.json");
@@ -22,9 +21,6 @@ const devConfig = {
       },
       shared: packageJson.dependencies,
     }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
   ]
 }
 
