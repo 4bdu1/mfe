487dd75efc61725c04fb71c5f69a9e99347b7815
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2022-06-28 08:17:21 +0000

    Add auth and fix public path in webpack

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createMemoryHistory, createBrowserHistory } from "history";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
 
-// mount function to start up the app
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory }) => {
   const history = defaultHistory || createMemoryHistory();
 
@@ -24,14 +24,16 @@ const mount = (el, { onNavigate, defaultHistory }) => {
   };
 };
 
-// if we are in dev and in isolation, call mount immediately
-if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
-// we are running through container and we should export the mount function
+// We are running through container
+// and we should export the mount function
 export { mount };
